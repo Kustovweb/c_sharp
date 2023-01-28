b61b43b8865c3bfa721b2cad0c4e402354da6c7c
Evgeniy <kustovweb@gmail.com>
2023-01-28 17:07:17 +0700

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2
+Console.WriteLine("Введите первое число");
+long a = Convert.ToInt64(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+long b = Convert.ToInt64(Console.ReadLine());
+
+
+if (a > b) Console.WriteLine("Максимальное число: " + a);
+else Console.WriteLine("Максимальное число: " + b);
+
+
