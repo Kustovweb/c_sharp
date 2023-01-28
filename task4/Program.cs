015d1adf61cda65259af56f4c9a1c2b373b2b7cc
Evgeniy <kustovweb@gmail.com>
2023-01-28 19:20:04 +0700

    Задача 8

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: >>>");
+long num = Convert.ToInt64(Console.ReadLine());
+int i = 2;
+while (i <= num)
+{
+  Console.WriteLine($"{i}");
+  i += 2;
+}
