9838e027398bf1e653bf73a10fb0263843ab6e51
Evgeniy <kustovweb@gmail.com>
2023-01-28 18:29:41 +0700

    Задача 6

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Является ли число четным? >>>");
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine(num % 2 == 0 ? "Четное" : "Нечетное");
+
