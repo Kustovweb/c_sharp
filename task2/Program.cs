45ba8e9ac6dea2ff6d24496d290da205030ef5ae
Evgeniy <kustovweb@gmail.com>
2023-01-28 17:54:38 +0700

    add code task 4

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Сравнить 3 числа >>>");
+Console.WriteLine("Введите число 1 :");
+long a = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Введите число 2 :");
+long b = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Введите число 3 :");
+long c = Convert.ToInt64(Console.ReadLine());
+
+long max = a;
+
+if (max < b)
+{
+  max = b;
+}
+if (max < c)
+{
+  max = c;
+}
+
+Console.WriteLine($"Максимальное число: {max}");
