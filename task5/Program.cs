a8dbd78f7e3fad5f38088fd2f8af6e88e7f530d3
Evgeniy <kustovweb@gmail.com>
2023-02-04 00:01:43 +0700

    Задача 10

---
+++ task5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: >>>");
+int num = int.Parse(Console.ReadLine());
+int num1 = (num / 10);
+
+Console.WriteLine($"{num} -> {num1 % 10}");
