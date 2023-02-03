c1136aa098b63dc9ebf768fcbe289868991f982c
Evgeniy <kustovweb@gmail.com>
2023-02-04 01:23:23 +0700

    Задача 13

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Clear();
+
+Console.WriteLine("Введите число >>>");
+
+string str = Convert.ToString(Console.ReadLine());
+
+if (str.Length < 3)
+{
+  Console.WriteLine("Третьей цифры нет");
+  return;
+}
+Console.WriteLine(str[2]);
