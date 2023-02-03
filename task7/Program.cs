6c8ff2f9f432f7b23c1765bb68190e3cbba64eaa
Evgeniy <kustovweb@gmail.com>
2023-02-04 02:44:19 +0700

    Задача 15

---
+++ task7/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+
+Console.WriteLine("Введите цифру >>>");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num == 6 || num == 7)
+{
+  Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");
+}
+
+
