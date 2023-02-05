42dd13dcd21ab12b3752ca53a23636e938bf7ce9
Evgeniy <kustovweb@gmail.com>
2023-02-05 10:16:09 +0700

    Задача 21

---
+++ task9/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координаты (x, y, z) точки А >>>");
+int ax = int.Parse(Console.ReadLine());
+int ay = int.Parse(Console.ReadLine());
+int az = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты (x, y, z) точки B >>>");
+int bx = int.Parse(Console.ReadLine());
+int by = int.Parse(Console.ReadLine());
+int bz = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+
+Console.WriteLine($"{result:f2}");
