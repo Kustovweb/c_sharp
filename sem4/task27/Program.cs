a56458ba97e55763e4b9ad66fc29757ca278cd63
Evgeniy <kustovweb@gmail.com>
2023-02-09 22:25:23 +0700

    Семинар 4

---
+++ sem4/task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 27: Напишите программу,
+которая принимает на вход число и выдаёт сумму цифр в числе.
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+int GetSumm(int num)
+{
+  int result = 0;
+
+  while (num > 0)
+  {
+    result += num % 10;
+    num /= 10;
+  }
+  return result;
+}
+
+
+Console.WriteLine($"{GetSumm(a)}");
---
+++ sem4/task29/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 29: Напишите программу,
+которая задаёт массив из 8 элементов и выводит их на экран.
+*/
+
+Console.Clear();
+int a = int.Parse(Console.ReadLine() ?? "0");
+int[] array = GetRandomArray(a);
+Console.Write($"[");
+for (int i = 0; i < a - 1; i++)
+{
+  Console.Write($"{array[i]}, ");
+
+}
+Console.Write($"{array[a - 1]}]");
+
+int[] GetRandomArray(int num)
+{
+  int[] arr = new int[num];
+  for (int i = 0; i < num; i++)
+  {
+    arr[i] = new Random().Next(1, 100);
+  }
+  return arr;
+}
