7d21585091125bd0e727c396af491781b05d3886
Evgeniy <kustovweb@gmail.com>
2023-02-18 22:15:53 +0700

    Семинар 6

---
+++ sem6/task41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите числа через пробел");
+string strNum = Console.ReadLine()!;
+int[] GetArrayFromString(string stringArray) {
+  string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+  int[] res = new int[nums.Length];
+
+  for (int i = 0; i < nums.Length; i++) {
+      res[i] = int.Parse(nums[i]);
+  }
+  return res;
+}
+
+int GetCountPositive(int[] arr) {
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++) {
+    if (arr[i] > 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+Console.WriteLine($"{GetCountPositive(GetArrayFromString(strNum))}");
---
+++ sem6/task43/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите значения точек через enter");
+Console.WriteLine("Введите b1");
+double b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = int.Parse(Console.ReadLine());
+int x = 0;
+int y = 1;
+double[] GetIntersectionPoint(double b1, double k1, double b2, double k2) {
+  double[] coord = new double[2];
+   coord[x] = (b2 - b1) / (k1 - k2);
+   coord[y] = k2 * coord[x] + b2;
+  return coord;
+}
+
+Console.WriteLine($"({String.Join(", ", GetIntersectionPoint(b1, k1, b2, k2))})");
